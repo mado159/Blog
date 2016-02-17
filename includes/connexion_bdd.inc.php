522e34672ca2991419eecec03492aa39429d893e
mado159 <madofrance59@gmail.com>
2016-02-17 18:57:22 +0100

    ajout gestion d'erreur pour la connexion base de donnée

--- includes/connexion_bdd.inc.php
+++ includes/connexion_bdd.inc.php
@@ -1,3 +1,10 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : ' .  $e->getMessage());
+	}
 ?>
