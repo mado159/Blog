b3b0b95571caead9a5c43f8eae566f7bd1f4ea18
mado159 <madofrance59@gmail.com>
2016-02-17 18:44:13 +0100

    page d'accueil

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Projet Blog php">
+    <meta name="author" content="Maxime Gonzalez">
+
+
+    <title>Blog Maxime Gonzalez</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Maxime Gonzalez</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><button class="btn btn-success">Se connecter</button></a></li>
+      <li><a href="#"><button class="btn btn-success">S'inscrire</button></a></li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Bonjour !</h1>
+        <p>Voici mon blog ! a remplir ! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores esse, molestiae tempore hic sint harum beatae quam incidunt dolorum et.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Article 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis maxime iste totam ipsa voluptatem eligendi mollitia sed neque accusamus dignissimos eaque laboriosam aut deleniti, quod? Autem, doloremque, quaerat. Tempore neque repellendus assumenda ipsam optio, laudantium, aspernatur? Aspernatur ratione accusamus, libero distinctio eos tenetur cumque pariatur soluta minus aut nihil culpa! </p>
+        </div>
+        <div class="col-md-4">
+          <h2>Artcile 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore architecto ad ipsum nobis sunt molestias, voluptatibus temporibus quam nihil est. Reiciendis alias, doloribus rem nesciunt tempore facere necessitatibus laborum accusamus esse quis quibusdam enim debitis delectus eius, similique omnis ex aliquam perferendis nihil. Unde facere vel, omnis, dignissimos eum doloribus? </p>
+       </div>
+        <div class="col-md-4">
+          <h2>Article 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odio at dignissimos, nemo temporibus laboriosam commodi, aut modi cum consequatur voluptates qui ea earum, optio mollitia consequuntur ut harum, fugit soluta laudantium veniam. Eos reprehenderit incidunt nihil, ipsa, nisi quas rem, voluptas consectetur debitis neque asperiores officia nobis delectus deserunt!</p>
+        </div>
+      </div>
+      <hr>
+      <footer>
+        <p>&copy; Maxime Gonzalez.</p>
+      </footer>
+    </div>
+
+    <script src="assets/js/jquery-1.12.0.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
